5afbc5e40198b55e5e2539a451df569f46a67627
Aleh Suprunovich <br@ahlamon.org>
2014-10-06 13:18:57 +0300

    Exercise 1.24

---
+++ ex1.24.scm
@@ -0,0 +1,44 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 10)
+      (report-prime (- (runtime) start-time))
+      false))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  true)
+
+(define (search-for-primes start count)
+  (cond ((even? start) (search-for-primes (+ start 1) count))
+	((> count 0) (if (timed-prime-test start)
+		       (search-for-primes (+ start 2) (- count 1))
+		       (search-for-primes (+ start 2) count)))))
+
+(search-for-primes 1000 3)
+(search-for-primes 10000 3)
+(search-for-primes 100000 3)
+(search-for-primes 1000000 3)
