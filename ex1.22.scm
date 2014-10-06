53d7a690f9acedb6a635584bad5a5efee412be88
Aleh Suprunovich <br@ahlamon.org>
2014-10-06 13:10:09 +0300

    Exercise 1.22

---
+++ ex1.22.scm
@@ -0,0 +1,40 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))
+      #f))
+
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  #t)
+
+(define (search-for-primes start count)
+  (cond ((even? start) (search-for-primes (+ start 1) count))
+	((> count 0) (if (timed-prime-test start)
+		       (search-for-primes (+ start 2) (- count 1))
+		       (search-for-primes (+ start 2) count)))))
+
+(search-for-primes 1000 3)
+(search-for-primes 10000 3)
+(search-for-primes 100000 3)
+(search-for-primes 1000000 3)
