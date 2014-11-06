a27c1567f37216a78dfa892d7935d43ea732b44f
Aleh Suprunovich <br@ahlamon.org>
2014-11-06 15:57:35 +0300

    Whitespace cleanup

--- ex2.8.scm
+++ ex2.8.scm
@@ -11,7 +11,7 @@
                    (max p1 p2 p3 p4))))
 
 (define (div-interval x y)
-  (mul-interval x 
+  (mul-interval x
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
 
