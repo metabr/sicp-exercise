4b78daf8646dd034223000a800ca64fbe14766c0
Aleh Suprunovich <br@ahlamon.org>
2014-11-06 15:56:07 +0300

    Exercise 2.10

---
+++ ex2.10.scm
@@ -0,0 +1,30 @@
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (if (or (and (negative? (lower-bound y)) (positive? (upper-bound y)))
+	  (and (positive? (lower-bound y)) (negative? (upper-bound y)))
+	  (zero? (lower-bound y))
+	  (zero? (upper-bound y)))
+    (error "Divider interval spans across 0" y)
+    (mul-interval x
+		  (make-interval (/ 1.0 (upper-bound y))
+				 (/ 1.0 (lower-bound y))))))
+
+(define (make-interval a b) (cons a b))
+
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(div-interval (make-interval 1 1.5) (make-interval -1 -1.3))
+(div-interval (make-interval -1 2) (make-interval 0 1))
+(div-interval (make-interval 2.2 3.3) (make-interval -1 2.1))
