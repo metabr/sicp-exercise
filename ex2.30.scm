4d25ed7c9a9f572630bddb251c7656314d06ff64
Aleh Suprunovich <br@ahlamon.org>
2014-11-14 15:10:20 +0300

    Exercise 2.30

---
+++ ex2.30.scm
@@ -0,0 +1,12 @@
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+	 (if (pair? sub-tree)
+	   (square-tree sub-tree)
+	   (* sub-tree sub-tree)))
+       tree))
+
+(newline)
+(display (square-tree
+	   (list 1
+		 (list 2 (list 3 4) 5)
+		 (list 6 7))))
