f76aed0b2dfab91f74f4d7b28aa42e4d1bc0d6a5
Aleh Suprunovich <br@ahlamon.org>
2014-12-24 00:22:06 +0300

    Exercise 2.48

---
+++ chapter2/ex2.48.scm
@@ -0,0 +1,29 @@
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor vect)
+  (car vect))
+
+(define (ycor vect)
+  (cdr vect))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor v1) (xcor v2))
+	     (+ (ycor v1) (ycor v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor v1) (xcor v2))
+	     (- (ycor v1) (ycor v2))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor v))
+	     (* s (ycor v))))
+
+(define (make-segment start-segment end-segment)
+  (cons start-segment end-segment))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
