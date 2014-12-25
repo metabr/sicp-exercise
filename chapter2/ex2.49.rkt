8b1ff2a11f182d35845857da46ee18e4e9a6fa8f
Aleh Suprunovich <br@ahlamon.org>
2014-12-25 15:23:05 +0300

    Exercise 2.49, parts c and d

--- chapter2/ex2.49.rkt
+++ chapter2/ex2.49.rkt
@@ -25,8 +25,45 @@
 (paint x)
 
 ; c. The painter that draws a diamond shape by connecting the midpoints of the sides of the frame.
+(define (diamond frame)
+  ((segments->painter (list (make-segment (make-vect 0 0.5) (make-vect 0.5 1))
+                            (make-segment (make-vect 0.5 1) (make-vect 1 0.5))
+                            (make-segment (make-vect 1 0.5) (make-vect 0.5 0))
+                            (make-segment (make-vect 0.5 0) (make-vect 0 0.5))))
+   frame))
+
+(paint diamond)
 
 ; d. The wave painter.
+(define (wave frame)
+  ((segments->painter (list (make-segment (make-vect 0.4 0) (make-vect 0.5 0.28))
+			    (make-segment (make-vect 0.5 0.28) (make-vect 0.6 0))
+			    (make-segment (make-vect 0.744 0) (make-vect 0.6 0.44))
+			    (make-segment (make-vect 0.6 0.44) (make-vect 1 0.136))
+			    (make-segment (make-vect 1 0.36) (make-vect 0.736 0.64))
+			    (make-segment (make-vect 0.736 0.64) (make-vect 0.6 0.64))
+			    (make-segment (make-vect 0.6 0.64) (make-vect 0.656 0.84))
+			    (make-segment (make-vect 0.656 0.84) (make-vect 0.6 1))
+			    (make-segment (make-vect 0.4 1) (make-vect 0.344 0.84))
+			    (make-segment (make-vect 0.344 0.84) (make-vect 0.4 0.64))
+			    (make-segment (make-vect 0.4 0.64) (make-vect 0.296 0.64))
+			    (make-segment (make-vect 0.296 0.64) (make-vect 0.136 0.576))
+			    (make-segment (make-vect 0.136 0.576) (make-vect 0 0.864)) ;
+			    (make-segment (make-vect 0 0.640) (make-vect 0.136 0.336)) ;
+			    (make-segment (make-vect 0.136 0.336) (make-vect 0.296 0.58))
+			    (make-segment (make-vect 0.296 0.58) (make-vect 0.336 0.48))
+			    (make-segment (make-vect 0.336 0.48) (make-vect 0.256 0))
+			    )
+		      )
+   frame))
+
+(paint wave)
+
+(define wave2 (beside wave (flip-vert wave)))
+(define wave4 (below wave2 wave2))
+
+(paint wave2)
+(paint wave4)
 
 ; Testing with procedures from 2.44.
 (define (corner-split painter n)
@@ -54,4 +91,5 @@
 
 (paint (corner-split outline 7))
 (paint (corner-split x 7))
+(paint (corner-split diamond 7))
 (paint (up-split x 10))
